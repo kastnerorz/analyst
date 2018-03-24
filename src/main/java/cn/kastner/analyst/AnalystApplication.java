65e82511611c6e1ac88a30915798d9cebe3c79ce
kastnerorz <djzhorz@gmail.com>
2018-03-24 10:23:12 +0800

    Initial commit

---
+++ src/main/java/cn/kastner/analyst/AnalystApplication.java
@@ -0,0 +1,12 @@
+package cn.kastner.analyst;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnalystApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnalystApplication.class, args);
+	}
+}
---
+++ src/test/java/cn/kastner/analyst/AnalystApplicationTests.java
@@ -0,0 +1,16 @@
+package cn.kastner.analyst;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AnalystApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
