fbc63e10bf07fac00ae153e963525fcf82baffff
kastnerorz <djzhorz@gmail.com>
2018-03-24 15:50:58 +0800

    add live reload server add vue add element-ui add index

---
+++ src/main/java/cn/kastner/analyst/Controller/HomeController.java
@@ -0,0 +1,14 @@
+package cn.kastner.analyst.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping(value = "/index")
+    public String index() {
+        return "index";
+    }
+
+}
