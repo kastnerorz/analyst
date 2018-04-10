ab775ad471f1d89b8657a42c588666f9d87460ba
kastnerorz <kastnerorz@gmail.com>
2018-04-10 15:10:14 +0800

    add ItemController add ItemRepository

---
+++ src/main/java/cn/kastner/analyst/controller/ItemController.java
@@ -0,0 +1,22 @@
+package cn.kastner.analyst.controller;
+
+import cn.kastner.analyst.domain.Item;
+import cn.kastner.analyst.repository.ItemRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ItemController {
+
+    @Autowired
+    ItemRepository itemRepository;
+
+    @RequestMapping(value = "/getBasicInfo")
+    public String getBasicInfo() {
+        String result = "";
+
+
+        return result;
+    }
+}
---
+++ src/main/java/cn/kastner/analyst/repository/ItemRepository.java
@@ -0,0 +1,8 @@
+package cn.kastner.analyst.repository;
+
+import cn.kastner.analyst.domain.Item;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ItemRepository extends JpaRepository<Item, String> {
+
+}
