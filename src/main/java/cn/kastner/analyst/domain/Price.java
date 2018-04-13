9c293834602f81528bad348a90a95c46675f20e7
kastnerorz <kastnerorz@gmail.com>
2018-04-13 23:54:57 +0800

    add price domain

--- src/main/java/cn/kastner/analyst/domain/Price.java
+++ src/main/java/cn/kastner/analyst/domain/Price.java
@@ -1,10 +1,45 @@
 package cn.kastner.analyst.domain;
 
+
 import javax.persistence.Entity;
+import javax.persistence.Id;
 import javax.persistence.Table;
+import java.util.UUID;
 
 @Entity
 @Table(name = "price")
 public class Price {
 
+    @Id
+    private String id;
+    private String content;
+    private String marketId;
+
+    public Price () {
+        id = UUID.randomUUID().toString();
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getMarketId() {
+        return marketId;
+    }
+
+    public void setMarketId(String marketId) {
+        this.marketId = marketId;
+    }
 }
