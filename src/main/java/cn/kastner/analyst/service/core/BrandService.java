876939941e32ee055cfeb2de27df6dfb00c92a6b
kastnerorz <kastnerorz@gmail.com>
2018-05-10 14:20:07 +0800

    啦啦啦

--- src/main/java/cn/kastner/analyst/service/core/BrandService.java
+++ src/main/java/cn/kastner/analyst/service/core/BrandService.java
@@ -7,12 +7,42 @@ import java.util.List;
 public interface BrandService {
 
     /**
+     * create a brand
+     *
      * @param brand
      * @return brand
      */
     Brand insertByBrand(Brand brand);
 
-    
+    /**
+     * delete a brand
+     *
+     * @param brandId
+     * @return brand
+     */
+    Brand delete(Long brandId);
+
+    /**
+     * update a brand
+     *
+     * @param brandId
+     * @return brand
+     */
+    Brand update(Long brandId);
+
+    /**
+     * retrieve a brand
+     *
+     * @param brandId
+     * @return brand
+     */
+    Brand findById(Long brandId);
+
+    /**
+     * retrieve all brands
+     *
+     * @return brand list
+     */
     List<Brand> findAll();
 
 
--- src/main/java/cn/kastner/analyst/service/core/impl/BrandServiceImpl.java
+++ src/main/java/cn/kastner/analyst/service/core/impl/BrandServiceImpl.java
@@ -1,4 +1,46 @@
 package cn.kastner.analyst.service.core.impl;
 
-public class BrandServiceImpl {
+import cn.kastner.analyst.domain.Brand;
+import cn.kastner.analyst.repository.BrandRepository;
+import cn.kastner.analyst.service.core.BrandService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class BrandServiceImpl implements BrandService {
+
+    final private
+    BrandRepository brandRepository;
+
+    @Autowired
+    public BrandServiceImpl(BrandRepository brandRepository) {
+        this.brandRepository = brandRepository;
+    }
+
+    @Override
+    public Brand insertByBrand(Brand brand) {
+        return null;
+    }
+
+    @Override
+    public Brand delete(Long brandId) {
+        return null;
+    }
+
+    @Override
+    public Brand update(Long brandId) {
+        return null;
+    }
+
+    @Override
+    public Brand findById(Long brandId) {
+        return null;
+    }
+
+    @Override
+    public List<Brand> findAll() {
+        return null;
+    }
 }
