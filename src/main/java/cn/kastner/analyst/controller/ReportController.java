6deb70d3e5e9a157c2eebbcf0d40faeea6c6668d
Sylvia <710573896@qq.com>
2018-05-30 23:48:51 +0800

    report的价格区间销量和品牌的平均价格

--- src/main/java/cn/kastner/analyst/controller/ReportController.java
+++ src/main/java/cn/kastner/analyst/controller/ReportController.java
@@ -34,7 +34,9 @@ public class ReportController {
         this.paramService = paramService;
     }
 
+
     @RequestMapping(value = "/getTopTenSaleAndItemNumByCategoryId")
+    //市场占比和品牌的商品总数和品牌的平均价格
     public NetResult getTopTenSaleAndItemNumByCategoryId(@RequestParam Long categoryId) {
         List<Brand> brands = brandService.findAll();   //All brand
         Long brandId = (long) 0;
@@ -45,19 +47,23 @@ public class ReportController {
             brandName = brand.getBrandZhName();
             List<Item> items = itemService.findByBrandIdAndCategoryId(brandId, categoryId);
             Long brandSale = (long) 0;
+            Double total = (double) 0;
             for (Item item : items) {
                 List<Price> prices = priceService.findByItem(item);
                 Long sum = (long) 0;
                 for (Price price : prices) {
                     sum = sum + price.getVolume();
+                    total = total + price.getPrice();
                 }
                 brandSale = brandSale + sum;
             }
+
             HashMap<String, Object> tmp = new HashMap<>();
             tmp.put("brandName", brandName);
             tmp.put("brandSale", brandSale);
             tmp.put("brandId", brandId);
             tmp.put("itemNum", items.size());
+            tmp.put("average",total/items.size());
             data.add(tmp);
 //            System.out.println(brand.getBrandZhName() + "的销售总量" + brandSale);
         }
@@ -84,7 +90,8 @@ public class ReportController {
             min = (double) 0;
             max = (double) 1000;
             while (min < 5999) {    //根据每个j区间遍历所有brand下的item总数
-                Long sum = (long) 0;
+                Long sum = (long) 0;   //每个区间的商品总数
+                Long saleTotal=(long)0; //每个区间的价格总销量
                 for (Brand brand : brands) {    //遍历每个brand得到该区间下的item
                     brandId = brand.getBrandId();
                     List<Item> items = itemService.findByBrandIdAndCategoryId(brandId, categoryId);
@@ -98,18 +105,24 @@ public class ReportController {
                     if (!itemIdList.isEmpty()) {   //找到每个brand下这个价格区间的商品数
                         List<Price> prices = priceService.findPriceListByPriceAndItem(min, max, itemIdList);
                         sum = sum + prices.size();
+                        for(Price price:prices){
+                            saleTotal=saleTotal+price.getVolume();
+                        }
+
 //                    System.out.println(sum);
                     }
                 }
                 HashMap<String, Object> tmp = new HashMap<>();
                 tmp.put("price", min);
                 tmp.put("sum", sum);
+                tmp.put("saleTotal",saleTotal);
                 priceSum.add(tmp);
                 min = min + 1000;
                 max = max + 1000;
             }
             //对7000+以上的价格遍历每个brand（感觉这样子复杂度太高了，哭了）
             Long sum = (long) 0;
+            Long saleTotal=(long) 0;
             for (Brand brand : brands) {
                 brandId = brand.getBrandId();
                 List<Item> items = itemService.findByBrandIdAndCategoryId(brandId, categoryId);
@@ -121,11 +134,15 @@ public class ReportController {
                 if (!itemIdList.isEmpty()) {
                     List<Price> prices = priceService.findPriceListByPriceMin(min, itemIdList);
                     sum = sum + prices.size();
+                    for(Price price:prices){
+                        saleTotal=saleTotal+price.getVolume();
+                    }
                 }
             }
             HashMap<String, Object> tmp = new HashMap<>();
             tmp.put("price", min);
             tmp.put("sum", sum);
+            tmp.put("saleTotal",saleTotal);
             priceSum.add(tmp);
         } else {
             //电脑3000,6000,9000,11000,14000;
@@ -133,6 +150,7 @@ public class ReportController {
             max = (double) 3000;
             while (min < 13999) {    //根据每个j区间遍历所有brand下的item总数
                 Long sum = (long) 0;
+                Long saleTotal=(long)0;
                 for (Brand brand : brands) {    //遍历每个brand得到该区间下的item
                     brandId = brand.getBrandId();
                     List<Item> items = itemService.findByBrandIdAndCategoryId(brandId, categoryId);
@@ -144,17 +162,22 @@ public class ReportController {
                     if (!itemIdList.isEmpty()) {   //找到每个brand下这个价格区间的商品数
                         List<Price> prices = priceService.findPriceListByPriceAndItem(min, max, itemIdList);
                         sum = sum + prices.size();
+                        for(Price price:prices){
+                            saleTotal=saleTotal+price.getVolume();
+                        }
                     }
                 }
                 HashMap<String, Object> tmp = new HashMap<>();
                 tmp.put("price", min);
                 tmp.put("sum", sum);
+                tmp.put("saleTotal",saleTotal);
                 priceSum.add(tmp);
                 min = min + 3000;
                 max = max + 3000;
             }
             //对14000+以上的价格遍历每个brand（感觉这样子复杂度太高了，哭了）
             Long sum = (long) 0;
+            Long saleTotal=(long)0;
             for (Brand brand : brands) {
                 brandId = brand.getBrandId();
                 List<Item> items = itemService.findByBrandIdAndCategoryId(brandId, categoryId);
@@ -166,11 +189,15 @@ public class ReportController {
                 if (!itemIdList.isEmpty()) {
                     List<Price> prices = priceService.findPriceListByPriceMin(min, itemIdList);
                     sum = sum + prices.size();
+                    for(Price price:prices){
+                        saleTotal=saleTotal+price.getVolume();
+                    }
                 }
             }
             HashMap<String, Object> tmp = new HashMap<>();
             tmp.put("price", min);
             tmp.put("sum", sum);
+            tmp.put("saleTotal",saleTotal);
             priceSum.add(tmp);
         }
 
@@ -189,14 +216,14 @@ public class ReportController {
         List<HashMap<String, Object>> focus = new ArrayList<>();
         for (Demand demand : demands) {
             HashMap<String, Object> tmp = new HashMap<>();
-            tmp.put("name",demand.getContent());
-            tmp.put("flag",demand.getFlag());
+            tmp.put("name", demand.getContent());
+            tmp.put("flag", demand.getFlag());
             focus.add(tmp);
-            List<Param> params=paramService.findByDemandId(demand.getId());
-            for(Param param:params){
+            List<Param> params = paramService.findByDemandId(demand.getId());
+            for (Param param : params) {
                 HashMap<String, Object> tmp2 = new HashMap<>();
-                tmp2.put("name",param.getValue());
-                tmp2.put("flag",param.getFlag());
+                tmp2.put("name", param.getValue());
+                tmp2.put("flag", param.getFlag());
                 focus.add(tmp2);
             }
         }
@@ -207,9 +234,10 @@ public class ReportController {
                 return (int) ((long) o2.get("flag") - (long) o1.get("flag"));
             }
         });
-        netResult.data=focus;
-        netResult.status=0;
+        netResult.data = focus;
+        netResult.status = 0;
         return netResult;
     }
+
 
 }
